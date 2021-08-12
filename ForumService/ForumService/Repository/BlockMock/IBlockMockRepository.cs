c3f84d9dd6e9374bfa41cb11db735c45144e19f1
saraallazarevic <sara.lola.2@gmail.com>
2021-08-12 12:37:00 +0200

    Dodavanje mock objekata

---
+++ ForumService/ForumService/Repository/BlockMock/IBlockMockRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumService.Repository.BlockMock
+{
+    public interface IBlockMockRepository
+    {
+        List<int> GetBlockedUsers(int userId);
+
+        /// <summary>
+        /// Provera da li je ulogovani korisnik blokirao drugog korisnika
+        /// </summary>
+        /// <param name="userId">Id ulogovanog</param>
+        /// <param name="blockedId">Id korisnika za kog se proverava da li je blokiran</param>
+        /// <returns></returns>
+        public bool CheckDidIBlockUser(int userId, int blockedId);
+    }
+}
---
+++ ForumService/ForumService/Repository/FollowingMock/IFollowingMockRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumService.Repository.FollowingMock
+{
+    interface IFollowingMockRepository
+    {
+        List<int> GetFollowedUsers(int userId);
+
+        /// <summary>
+        /// Provera koga prati ulogovani korisnik
+        /// </summary>
+        /// <param name="userId">Id ulogovanog korisnika</param>
+        /// <param name="followingId">Id korisnika za kog se proverava da li ga user prati</param>
+        /// <returns></returns>
+        public bool CheckDoIFollowUser(int userId, int followingId);
+    }
+}
