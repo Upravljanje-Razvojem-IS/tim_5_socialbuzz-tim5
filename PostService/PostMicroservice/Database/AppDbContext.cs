2e53594c5dfd15dd03f3eeffdf222e0ffe1e6234
Milica775 <milica.despotovic981@gmail.com>
2021-08-17 17:28:49 +0200

    Renaming the project

---
+++ PostService/PostMicroservice/Database/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PostMicroservice.Entities;
+
+namespace PostMicroservice.Database
+{
+    public class AppDbContext : DbContext
+    {
+       public AppDbContext(DbContextOptions<AppDbContext> options) :
+            base(options)
+        {
+            
+        }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Image> Images { get; set; }
+    }
+}
---
+++ PostService/PostMicroservice/Entities/Content.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PostMicroservice.Entities
+{
+    /// <summary>
+    /// Entity class which represents content of the post
+    /// </summary>
+    public class Content
+    {
+        /// <summary>
+        /// Content ID.
+        /// </summary>
+        [Key]
+        [Required]
+        public Guid ContentId { get; set; }
+
+        /// <summary>
+        /// Title of the content.
+        /// </summary>
+        [StringLength(50)]
+        [Required]
+        public string Title { get; set; }
+        
+        /// <summary>
+        /// Text of the content.
+        /// </summary>
+        [StringLength(300)]
+        [Required]
+        public string Text { get; set; }
+
+        /// <summary>
+        /// The possibility of replacing the content that is published.
+        /// </summary>
+        [Required]
+        public bool Replacement { get; set; }
+
+        /// <summary>
+        /// The state of the content that is published.
+        /// </summary>
+        [Required]
+        public string State { get; set; }
+
+        /// <summary>
+        /// ID of item for sale.
+        /// </summary>
+        [Required]
+        public int ItemForSaleID { get; set; }
+
+
+
+    }
+}
