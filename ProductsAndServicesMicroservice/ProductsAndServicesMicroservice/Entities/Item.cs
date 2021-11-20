7b27dd906c81fa3778b23e6bf81a411ce5e1ed4f
VericaL98 <lulic.it6.2017@uns.ac.rs>
2021-11-20 16:08:03 +0100

    initial commit

---
+++ ProductsAndServicesMicroservice/ProductsAndServicesMicroservice/Entities/Item.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsAndServicesMicroservice.Entities
+
+/// <summary>
+/// Entity class which represents items that could be products or services
+/// </summary>
+{
+    public class Item
+    {
+        /// <summary>
+        /// An identifier for the item 
+        /// </summary>
+        [Key]
+        [Required]
+        public Guid ItemId { get; set; }
+
+        /// <summary>
+        /// Name of the item 
+        /// </summary>
+        [StringLength(50)]
+        [Required]
+        public String Name { get; set; }
+
+        /// <summary>
+        /// Description of the item 
+        /// </summary>
+        [StringLength(300)]
+        [Required]
+        public String Description { get; set; }
+
+        /// <summary>
+        /// Price of the item 
+        /// </summary>
+        [Required]
+        public String Price { get; set; }
+
+        /// <summary>
+        /// Id of the user who adds the item to the wall
+        /// </summary>
+        public Guid AccountId { get; set; }
+    }
+}
---
+++ ProductsAndServicesMicroservice/ProductsAndServicesMicroservice/Entities/PastPrice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsAndServicesMicroservice.Entities
+{
+
+    /// <summary>
+    /// Entity class which represents model where is store last price of item for sale
+    /// </summary>
+    public class PastPrice
+    {
+        /// <summary>
+        /// An identifier for the past price
+        /// </summary>
+        [Key]
+        [Required]
+        public int PastPriceId { get; set; }
+
+        /// <summary>
+        /// Item id to which the previous price applies
+        /// </summary>
+        [ForeignKey("ItemId")]
+        public Guid ItemId { get; set; }
+
+        /// <summary>
+        /// Amount of past price
+        /// </summary>
+        [Required]
+        public String Price { get; set; }
+    }
+}
